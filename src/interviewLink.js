700d4cc5ed4f557f1c172379a96b4255f224c828
Malorie Casimir <malorie.casimir@flatironschool.com>
2019-05-16 12:09:56 -0400

    PROXY

--- src/interviewLink.js
+++ src/interviewLink.js
@@ -1,21 +1,23 @@
-import fetch from 'node-fetch'
 import generateToken from './utils/tokenGenerator.js'
 
 const payload = {
 	"jti": "d8661a14-4b7c-5fda-2227-9b055fcf5b10", // Random GUID
-	"iss": "YOUR_API_KEY",
+	"iss": "d9eaa136-20d5-0f51-60e1-943efbebd493",
 	"iat": 1519343714, // Current NumericDate
 	"sub": "44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a", // SHA256 hash request body
 	"exp": 1519343724 // Current NumericDate + 10 seconds
 }
 
+const proxy = 'https://cors-anywhere.herokuapp.com/'
+
 const getInterviewLink = () => {
-	return fetch('https://interviews.skype.com/api/interviews', {
+	return fetch(proxy + 'https://interviews.skype.com/api/interviews', {
 	  method: 'POST',
 	  headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'POST',
 	    'Content-Type': 'application/json',
-	    'Authorization': 'Bearer ' + generateToken(JSON.stringify(payload)),
-			'Access-Control-Allow-Origin': 'http://localhost:3000'
+	    'Authorization': 'Bearer ' + generateToken()
 	  },
 	  body: JSON.stringify(payload)
 	})
--- src/utils/tokenGenerator.js
+++ src/utils/tokenGenerator.js
@@ -7,7 +7,7 @@ function generateToken(content) {
   return jwt.sign({
     jti: Guid.raw(),
     iss: "d9eaa136-20d5-0f51-60e1-943efbebd493",
-    sub: sha256(content),
+    sub: sha256("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkODY2MWExNC00YjdjLTVmZGEtMjIyNy05YjA1NWZjZjViMTAiLCJpc3MiOiIwZWQwZTAxYi00ZjhmLTkzZWUtNDVkZS05Njk1OTU0YTQ0YTkiLCJzdWIiOiI0NDEzNmZhMzU1YjM2NzhhMTE0NmFkMTZmN2U4NjQ5ZTk0ZmI0ZmMyMWZlNzdlODMxMGMwNjBmNjFjYWFmZjhhIiwiZXhwIjoxNTE5MzQzNzI0LCJpYXQiOjE1MTkzNDM3MTR9.4KSookeDh2d_Vujy_bUiA2n0yKY39inaIc1laWjDt6Q"),
     exp: Math.floor(Date.now() / 1000) + 10
   }, "838b3313-6092-5cc8-3a62-fd02f14a80eb")
 }
