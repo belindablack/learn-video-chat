cc3d90f6e37ed45e52daa725c0590f71b8d2766c
Natalie Olivo <nolivo@NYC-US09400-MBA.fios-router.home>
2019-05-16 12:50:54 -0400

    Date fiddling

--- src/interviewLink.js
+++ src/interviewLink.js
@@ -1,11 +1,11 @@
 import generateToken from './utils/tokenGenerator.js'
+import sha256 from 'sha256'
 
 const payload = {
 	"jti": "d8661a14-4b7c-5fda-2227-9b055fcf5b10", // Random GUID
 	"iss": "d9eaa136-20d5-0f51-60e1-943efbebd493",
-	"iat": 1519343714, // Current NumericDate
-	"sub": "44136fa355b3678a1146ad16f7e8649e94fb4fc21fe77e8310c060f61caaff8a", // SHA256 hash request body
-	"exp": 1519343724 // Current NumericDate + 10 seconds
+	"iat": 1558025227825, // Current NumericDate
+	"exp": 1558025227825 + 1000 // Current NumericDate + 10 seconds
 }
 
 const proxy = 'https://cors-anywhere.herokuapp.com/'
@@ -13,15 +13,13 @@ const proxy = 'https://cors-anywhere.herokuapp.com/'
 const getInterviewLink = () => {
 	return fetch(proxy + 'https://interviews.skype.com/api/interviews', {
 	  method: 'POST',
-	  headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': 'POST',
+	  headers: {		
 	    'Content-Type': 'application/json',
-	    'Authorization': 'Bearer ' + generateToken()
+	    'Authorization': 'Bearer ' + generateToken(JSON.stringify(payload))	    
 	  },
 	  body: JSON.stringify(payload)
 	})
-	.then(res => res.json())
+	.then(res => res)
 	.then(console.log)
 }
 
